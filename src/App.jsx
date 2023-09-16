0ba04fe1d76e02a4ac973ccc47bd15c68fe904e5
Asaad Balla <asaadballah@gmail.com>
2023-09-16 23:25:18 +0800

    added term&conditions

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Hero from "./components/Hero";
 import Demo from "./components/Demo";
 import Footer from "./components/Footer";
+import Terms from './pages/terms'; // Ensure this path points to your Terms component
+import Privacy from './pages/privacy'; // Ensure this path points to your Privacy component
 
 import "./App.css";
 
 const App = () => {
   return (
-    <main>
-      <div className='main'>
-        <div className='gradient' />
-      </div>
+    <BrowserRouter>
+      <main>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
 
-      <div className='app'>
-        <Hero />
-        <Demo />
-        <Footer />
-      </div>
-    </main>
+        <div className='app'>
+          <Routes>
+            <Route path="/" element={<><Hero /><Demo /><Footer /></>} />
+            <Route path="/terms" element={<Terms />} />
+            <Route path="/privacy" element={<Privacy />} />
+          </Routes>
+        </div>
+      </main>
+    </BrowserRouter>
   );
 };
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,11 @@ const Footer = () => {
   return (
     <footer className='w-full flex justify-center items-center flex-col py-3 mt-6' dir="rtl">
       <p className='text-gray-600'>هذا المنتج مقدم من <span className='font-bold orange_gradient'>249Technologies</span></p>
+      <div className="w-full flex justify-center items-center flex-col py-3 mt-6" dir="rtl">
+          {/* <a href="/terms" className="text-blue-500 hover:underline mr-4">الشروط والأحكام</a> */}
+          <span><a href="/terms" className="text-blue-500 hover:underline mr-4">الشروط والأحكام</a></span>
+          <span><a href="/privacy" className="text-blue-500 hover:underline">الخصوصية</a></span>
+      </div>
     </footer>
   );
 };
